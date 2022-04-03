1178d2b2a620bf0420dd70e761c3d00b86db0e4f
sintemal <sebastian@sebastianfritsch.com>
2022-04-03 23:34:52 +0200

    update scripts from placede

--- scripts/generate_json.py
+++ scripts/generate_json.py
@@ -1,4 +1,3 @@
-#! /usr/bin/env python3
 import argparse
 import pathlib
 import json
@@ -6,39 +5,38 @@ import toml
 from PIL import Image
 
 COLOR_MAPPINGS = {
+	'#6D001A': 0,
 	'#BE0039': 1,
-    '#6D001A': 0,
-    '#BE0039': 1,
-    '#FF4500': 2,
-    '#FFA800': 3,
-    '#FFD635': 4,
-    '#FFF8B8': 5,
-    '#00A368': 6,
-    '#00CC78': 7,
-    '#7EED56': 8,
-    '#00756F': 9,
-    '#009EAA': 10,
-    '#00CCC0': 11,
-    '#2450A4': 12,
-    '#3690EA': 13,
-    '#51E9F4': 14,
-    '#493AC1': 15,
-    '#6A5CFF': 16,
-    '#94B3FF': 17,
-    '#811E9F': 18,
-    '#B44AC0': 19,
-    '#E4ABFF': 20,
-    '#DE107F': 21,
-    '#FF3881': 22,
-    '#FF99AA': 23,
-    '#6D482F': 24,
-    '#9C6926': 25,
-    '#FFB470': 26,
-    '#000000': 27,
-    '#515252': 28,
-    '#898D90': 29,
-    '#D4D7D9': 30,
-    '#FFFFFF': 31
+	'#FF4500': 2,
+	'#FFA800': 3,
+	'#FFD635': 4,
+	'#FFF8B8': 5,
+	'#00A368': 6,
+	'#00CC78': 7,
+	'#7EED56': 8,
+	'#00756F': 9,
+	'#009EAA': 10,
+	'#00CCC0': 11,
+	'#2450A4': 12,
+	'#3690EA': 13,
+	'#51E9F4': 14,
+	'#493AC1': 15,
+	'#6A5CFF': 16,
+	'#94B3FF': 17,
+	'#811E9F': 18,
+	'#B44AC0': 19,
+	'#E4ABFF': 20,
+	'#DE107F': 21,
+	'#FF3881': 22,
+	'#FF99AA': 23,
+	'#6D482F': 24,
+	'#9C6926': 25,
+	'#FFB470': 26,
+	'#000000': 27,
+	'#515252': 28,
+	'#898D90': 29,
+	'#D4D7D9': 30,
+	'#FFFFFF': 31
 }
 
 def hex_to_col(hex_str, alpha=0xff):
@@ -63,22 +61,35 @@ def find_closest_index(color):
             closest = key
     return colors[closest]
 
-def create_structure(image, startx, starty, priority, ignore_colors):
+def create_structure(image, priority_mask, startx, starty, structure_priority, ignore_colors, used_pixels):
     pixels = []
+
+    def add_pixel(x, y, color, pixel_priority):
+        if (x,y) in used_pixels:
+            return
+        used_pixels.add((x,y))
+        pixels.append({
+                        "x": x,
+                        "y": y,
+                        "color": color,
+                        "priority": pixel_priority,
+                    })
+
     with Image.open(image) as img:
+        priority_img = Image.open(priority_mask) if priority_mask else None
         for x in range(img.size[0]):
             for y in range(img.size[1]):
                 color = img.getpixel((x, y))
                 if color in ignore_colors:
                     continue
-                pixels.append({
-                    "x": startx + x,
-                    "y": starty + y,
-                    "color": find_closest_index(color),
-                    "priority": priority,
-                })
+                pixel_priority = priority_img.getpixel((x, y))[0] if priority_img else 128
+                add_pixel(startx + x, starty + y, find_closest_index(color), pixel_priority)
+        
+        if priority_img:
+            priority_img.close()
+
     return {
-            "priority": priority,
+            "priority": structure_priority,
             "pixels": pixels,
             }
 
@@ -97,11 +108,15 @@ if __name__ == "__main__":
         "structures": {},
         "priorities": config["priorities"],
     }
-    for struct in config["structure"]:
+
+    # Make sure we only place pixels once
+    used_pixels = set()
+    for struct in reversed(config["structure"]):
         file = struct["file"]
+        priority_file = struct.get("priority_file", None)
         name = struct["name"]
         print(f"Adding file {file} for structure {name}")
-        data["structures"][name] = create_structure(file, struct["startx"], struct["starty"], struct["priority"], ignore_colors)
+        data["structures"][name] = create_structure(file, priority_file, struct["startx"], struct["starty"], struct["priority"], ignore_colors, used_pixels)
     
     with open(args.output, "w") as f:
         f.write(json.dumps(data, indent=4))
--- scripts/render_json.py
+++ scripts/render_json.py
@@ -1,4 +1,3 @@
-#! /usr/bin/env python3
 from PIL import Image
 import argparse
 import pathlib
@@ -7,25 +6,55 @@ import json
 WIDTH = 2000
 HEIGHT = 2000
 
-colors = {1: (190, 0, 57, 255), 0: (109, 0, 26, 255), 2: (255, 69, 0, 255), 3: (255, 168, 0, 255), 4: (255, 214, 53, 255), 5: (255, 248, 184, 255), 6: (0, 163, 104, 255), 7: (0, 204, 120, 255), 8: (126, 237, 86, 255), 9: (0, 117, 111, 255), 10: (0, 158, 170, 255), 11: (0, 204, 192, 255), 12: (36, 80, 164, 255), 13: (54, 144, 234, 255), 14: (81, 233, 244, 255), 15: (73, 58, 193, 255), 16: (
-    106, 92, 255, 255), 17: (148, 179, 255, 255), 18: (129, 30, 159, 255), 19: (180, 74, 192, 255), 20: (228, 171, 255, 255), 21: (222, 16, 127, 255), 22: (255, 56, 129, 255), 23: (255, 153, 170, 255), 24: (109, 72, 47, 255), 25: (156, 105, 38, 255), 26: (255, 180, 112, 255), 27: (0, 0, 0, 255), 28: (81, 82, 82, 255), 29: (137, 141, 144, 255), 30: (212, 215, 217, 255), 31: (255, 255, 255, 255)}
-
+colors = {
+    0: (0x6D, 0x00, 0x1A, 0xFF),
+    1: (0xBE, 0x00, 0x39, 0xFF),
+    2: (0xFF, 0x45, 0x00, 0xFF),
+    3: (0xFF, 0xA8, 0x00, 0xFF),
+    4: (0xFF, 0xD6, 0x35, 0xFF),
+    5: (0xFF, 0xF8, 0xB8, 0xFF),
+    6: (0x00, 0xA3, 0x68, 0xFF),
+    7: (0x00, 0xCC, 0x78, 0xFF),
+    8: (0x7E, 0xED, 0x56, 0xFF),
+    9: (0x00, 0x75, 0x6F, 0xFF),
+    10: (0x00, 0x9E, 0xAA, 0xFF),
+    11: (0x00, 0xCC, 0xC0, 0xFF),
+    12: (0x24, 0x50, 0xA4, 0xFF),
+    13: (0x36, 0x90, 0xEA, 0xFF),
+    14: (0x51, 0xE9, 0xF4, 0xFF),
+    15: (0x49, 0x3A, 0xC1, 0xFF),
+    16: (0x6A, 0x5C, 0xFF, 0xFF),
+    17: (0x94, 0xB3, 0xFF, 0xFF),
+    18: (0x81, 0x1E, 0x9F, 0xFF),
+    19: (0xB4, 0x4A, 0xC0, 0xFF),
+    20: (0xE4, 0xAB, 0xFF, 0xFF),
+    21: (0xDE, 0x10, 0x7F, 0xFF),
+    22: (0xFF, 0x38, 0x81, 0xFF),
+    23: (0xFF, 0x99, 0xAA, 0xFF),
+    24: (0x6D, 0x48, 0x2F, 0xFF),
+    25: (0x9C, 0x69, 0x26, 0xFF),
+    26: (0xFF, 0xB4, 0x70, 0xFF),
+    27: (0x00, 0x00, 0x00, 0xFF),
+    28: (0x51, 0x52, 0x52, 0xFF),
+    29: (0x89, 0x8D, 0x90, 0xFF),
+    30: (0xD4, 0xD7, 0xD9, 0xFF),
+    31: (0xFF, 0xFF, 0xFF, 0xFF),
+}
 
 def hex_to_col(hex_str, alpha=0xff):
     assert hex_str[0] == "#" and len(hex_str) == 7
-
     def conv(s):
         return int(s, 16)
     return (conv(hex_str[1:3]), conv(hex_str[3:5]), conv(hex_str[5:7]))
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("files", nargs="*",
-                        type=pathlib.Path, default=["pixel.json"])
+    parser.add_argument("files", nargs="*", type=pathlib.Path, default=["pixel.json"])
     args = parser.parse_args()
 
     img = Image.new("RGBA", (WIDTH, HEIGHT), "#00000000")
+    img_overlay = Image.new("RGBA", (WIDTH * 3, HEIGHT * 3), "#00000000")
+    img_priority = Image.new("RGBA", (WIDTH, HEIGHT), "#00000000")
 
     for file in args.files:
         with open(file) as f:
@@ -37,10 +66,15 @@ if __name__ == "__main__":
                 x = pixel["x"]
                 y = pixel["y"]
                 color_index = pixel["color"]
+                priority = pixel["priority"]
                 if type(color_index) is str:
                     color = hex_to_col(color_index)
                     print(f"converted {color_index} to {color}")
                 else:
                     color = colors[color_index]
                 img.putpixel((x, y), color)
+                img_overlay.putpixel((x * 3 + 1, y * 3 + 1), color)
+                img_priority.putpixel((x, y), (priority, priority, priority))
     img.save("output.png")
+    img_overlay.save("overlay.png")
+    img_priority.save("priority.png")
